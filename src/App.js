d378723468a466082c1fbce825d5412d850b921b
Priaynk911 <panchalpriyank48@gmail.com>
2024-11-24 14:03:32 +0530

    add

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import RegisterPage from "./components/RegisterPage"; // Import the Register Pag
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app">
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        {/* Redirect any unmatched routes to the login page */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </div>
+  </Router>
   );
 }
 
