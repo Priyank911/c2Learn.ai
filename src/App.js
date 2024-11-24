661b04e524d50fbbcbeddbe647446e6fd345330d
Priaynk911 <panchalpriyank48@gmail.com>
2024-11-24 14:06:42 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage"; // Import the Register Page component
+import RegisterPage from "./components/RegisterPage";
 
 function App() {
   return (
     <Router>
-    <div className="app">
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        {/* Redirect any unmatched routes to the login page */}
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </div>
-  </Router>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          {/* Redirect any unmatched routes to the login page */}
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
